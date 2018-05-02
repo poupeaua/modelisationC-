b485184c79b81e97819407e296d22a932b0a551b
Alexandre Poupeau <alexandre.poupeau@grenoble-inp.org>
2018-05-02 19:08:40 +0200

    Modification gitignore pour que le build ne soit pas push

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /user/5/.base/poupeaua/home/Documents/2A/modelisation/TP1_balloua_poupeaua
-# Build directory: /user/5/.base/poupeaua/home/Documents/2A/modelisation/TP1_balloua_poupeaua/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(test)
